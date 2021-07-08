c44419586b388acb9e1839e99b399031d8b612ef
sathinenisnigdhareddy <sathineniandfamily5@gmail.com>
2021-07-08 16:36:41 +0530

    adding login page

--- backend/new_pro/new_app/urls.py
+++ backend/new_pro/new_app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('register/', views.registerPage, name="register"),]
+urlpatterns = [path('register/', views.registerPage, name="register"),
+path('login/', views.loginPage, name="login"),
+]
--- backend/new_pro/new_app/views.py
+++ backend/new_pro/new_app/views.py
@@ -24,5 +24,23 @@ def registerPage(request):
 
 			messages.success(request, 'Account was created for ' + username)
 
+			return redirect('login')
+
 	context = {'form':form}
 	return render(request, 'register.html', context)
+def loginPage(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password =request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+
+	context = {}
+	return render(request, 'login.html', context)
